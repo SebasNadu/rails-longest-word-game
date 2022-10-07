2d721dc40175f1d87f348c9b0f2eb8c3d6118b4c
sebastian navarro <sebastiannavarro.fl@gmail.com>
2022-10-07 11:58:26 +0200

    refactor

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,27 +1,32 @@
 require 'json'
 require 'open-uri'
 class GamesController < ApplicationController
-  
-  SHUF = ('A'..'Z').to_a.shuffle[0..9] 
+
+  def initialize 
+    super
+    @shuf = ('A'..'Z').to_a.shuffle[0..9] 
+  end
 
   def new
-    @letters = SHUF 
+    @letters = @shuf 
   end
 
   def score
     @word = params[:word].upcase
-    @letters = SHUF
-    @example = SHUF.join(', ')
+    @letters = @shuf
+    @example = @shuf.join(', ')
     @count = 0
     @found = find_word(params[:word])
     session[:score]
   end
   
+  private
+  
   def find_word(word)
     url = "https://wagon-dictionary.herokuapp.com/#{word}"
     dictionary_serialized = URI.open(url).read
     dictionary = JSON.parse(dictionary_serialized)
-    return dictionary["found"]
+    dictionary["found"]
   end
 
 end
